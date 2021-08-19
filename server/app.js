3fc70476b9d5d2c36bbf3dccbcf10974f8114264
sagar_2001 <sagarshaw2001@gmail.com>
2021-08-19 20:24:55 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,33 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", (req, res) => {
+dotenv.config({ path: "./.env" });
+
+const DB = process.env.DATABASE;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connection Successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const middleware = (re, res, next) => {
+  console.log("middleware working");
+  next();
+};
+
+app.get("/", middleware, (req, res) => {
   res.send("Hello Sagar");
 });
 
@@ -18,6 +44,6 @@ app.get("/signup", (req, res) => {
   res.send("This is signup");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
 });
